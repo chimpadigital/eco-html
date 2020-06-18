f6cf6889872278eec1a268c596ecb52bf4452a21
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2020-06-18 15:17:13 -0300

    agregando calendar y dando estilos

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,25 @@
-    $('#myCalendar').jalendar();
+$(function(){
+
+    $('#Calendar-1').jalendar({
+        color: '#FBF9F9',
+        lang: 'ES',
+        sundayStart: true,
+        dayWithZero: false,
+        dayColor: '#515150',
+        titleColor: '#0097D6',
+        weekColor: '#00B49D',
+        todayColor: '#fff'
+    });
+
+    $('#Calendar-2').jalendar({
+        color: '#FBF9F9',
+        lang: 'ES',
+        sundayStart: true,
+        dayWithZero: false,
+        dayColor: '#515150',
+        titleColor: '#0097D6',
+        weekColor: '#00B49D',
+        todayColor: '#fff'
+    });
+
+});
