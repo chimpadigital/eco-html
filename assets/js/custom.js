d019b0e41e154c88aed162395c2aad264e419b3c
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2020-06-25 23:14:42 -0300

    agregando links

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -24,4 +24,18 @@ $(function(){
 
 });
 
-$('[data-toggle="tooltip"]').tooltip()
+$('[data-toggle="tooltip"]').tooltip();
+
+$(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+      var destino = $(this.hash);
+      if (destino.length == 0) {
+        destino = $('a[name="' + this.hash.substr(1) + '"]');
+      }
+      if (destino.length == 0) {
+        destino = $('html');
+      }
+      $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+      return false;
+    });
+  });
